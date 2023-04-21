a665527017a93c5b5b992cded28e3106644e9560
ny2292000 <ny2292000@gmail.com>
2023-04-20 20:51:12 -0400

    Working both Class Library and Helper Functions Library lib0.cpp Starting to create a python package Separated process using subdirectories. Now working with add_dependency

---
+++ cmake/FindGALAXY_LIB.cmake
@@ -0,0 +1,13 @@
+find_library(GALAXY_LIB NAMES libGalaxyLibNonCuda PATHS ${CMAKE_SOURCE_DIR}/lib)
+message( " where to look for GalaxyLib " ${CMAKE_SOURCE_DIR}/lib))
+if(GALAXY_LIB)
+    set(GALAXY_LIB_FOUND TRUE)
+    message(STATUS "Found GalaxyLibNonCuda: ${GALAXY_LIB}")
+else()
+    set(GALAXY_LIB_FOUND FALSE)
+    message(FATAL_ERROR "Could not find GalaxyLibNonCuda")
+endif()
+
+# Export variables
+set(GALAXY_LIB ${GALAXY_LIB} CACHE FILEPATH "Path to GalaxyLibNonCuda library")
+set(GALAXY_LIB_FOUND ${GALAXY_LIB_FOUND} CACHE BOOL "Whether GalaxyLibNonCuda library was found")
